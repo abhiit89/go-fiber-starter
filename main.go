17f5aa646cff2d7a7f361e9f47ed66637b7a2b39
Abhinav Nath Gupta <abhi.aec89@gmail.com>
2022-10-15 13:40:01 +0530

    :sparkles: (Go Start) Added the Fiber Monitor
    Added the Fiber Monitor
    
    BREAKING CHANGE: None
    
    Closes

--- main.go
+++ main.go
@@ -2,9 +2,19 @@ package main
 
 import "github.com/gofiber/fiber/v2"
 import "github.com/gofiber/fiber/v2/middleware/monitor"
+import "github.com/gofiber/fiber/v2/middleware/logger"
+import "github.com/gofiber/fiber/v2/middleware/requestid"
+
 func main() {
 	app := fiber.New()
-        app.Get("/metrics", monitor.New(monitor.Config{Title: "MyService Metrics Page"}))
+	app.Use(requestid.New())
+
+	app.Use(logger.New(logger.Config{
+		// For more options, see the Config section
+		Format: "${pid} ${locals:requestid} ${status} - ${method} ${path}\n",
+	}))
+
+	app.Get("/metrics", monitor.New(monitor.Config{Title: "MyService Metrics Page"}))
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
