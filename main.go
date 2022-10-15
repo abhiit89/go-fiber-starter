6182a154e12efe3523fb5ba97c674d92d2fa6b61
Abhinav Nath Gupta <abhi.aec89@gmail.com>
2022-10-15 13:03:11 +0530

    :sparkles: (go-start) Added Support for fiber
    Added Support for fiber
    
    BREAKING CHANGE: Added Support for fiber
    
    Closes

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
-
+import "github.com/gofiber/fiber/v2"
+import "github.com/gofiber/fiber/v2/middleware/monitor"
 func main() {
-	fmt.Println("Hello World")
+	app := fiber.New()
+        app.Get("/metrics", monitor.New(monitor.Config{Title: "MyService Metrics Page"}))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":3000")
 }
